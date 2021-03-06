6ca2b09b4deb666ca2f4fac9ef21ed61e12c68ee
inset-cpu <1916601829@qq.com>
2021-03-06 15:27:44 +0800

    第二次

--- src/com/yr/GitHub/Main.java
+++ src/com/yr/GitHub/Main.java
@@ -2,8 +2,22 @@ package com.yr.GitHub;
 
 public class Main {
 
+    public void test(){
+
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
+
+
+
         System.out.println("ÖîÂÞÂå");
         System.out.println("1916601829");
+
     }
 }
